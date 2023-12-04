fb09b74dfb5d0af675ccee2501fe5b41fd847adf
Andrey Martynov <kubris@mail.ru>
2023-12-04 20:40:19 +0300

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp 	= require('gulp');
+
+// Tasks
+require('./gulp/dev.js');
+require('./gulp/docs.js');
+
+// === GULP RUN ===
+// developmen: gulp
+gulp.task('default', 
+	gulp.series('clean:dev', 
+	gulp.parallel('html:dev', 'sass:dev', 'images:dev', 'fonts:dev', 'files:dev', 'js:dev'),
+	gulp.parallel('server:dev', 'watch:dev')
+));
+
+// production: gulp docs
+gulp.task(
+	'docs', 
+	gulp.series('clean:docs', 
+		gulp.parallel('html:docs', 'sass:docs', 'images:docs', 'fonts:docs', 'files:docs', 'js:docs'),
+		gulp.parallel('server:docs')
+));
+// === end GULP RUN ===
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import mobileNav from './modules/mobile-nav.js';
+
+mobileNav();
+
+// import AirDatepicker from 'air-datepicker';
+// import 'air-datepicker/air-datepicker.css';
+// new AirDatepicker('#calendar')
